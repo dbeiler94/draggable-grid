8ef986156c07c8b330ed8b3e6fe6278ee2befabf
David Beiler <2010738399@stud.fh-kufstein.ac.at>
2021-05-11 17:22:00 +0200

    event listener on parent with condition

--- docs/index.js
+++ docs/index.js
@@ -64,10 +64,11 @@ function initDragAndDrop() {
     });
 }
 
+/*
 function initTouch() {
     // Instead of adding the eventListeners to the parent ul Element, they are added to the single li Elements
     // This is, because otherwise it would be able to drag the whole ul Element
-    //const draggableItemsContainer = document.querySelector('ul');
+    const draggableItemsContainer = document.querySelector('ul');
     const draggableItems = document.getElementsByTagName('li');
     let initialX = 0;
     let initialY = 0;
@@ -112,6 +113,61 @@ function initTouch() {
         });
     }
 }
+*/
+
+function initTouch() {
+    const draggableItemsContainer = document.querySelector('ul');
+    let initialX = 0;
+    let initialY = 0;
+    let lastX = 0;
+    let lastY = 0;
+
+    draggableItemsContainer.addEventListener('touchstart', (e) => {
+        // After class i changed the event listeners so that it will be checked if the element is an li element
+        // Now i can add the event listeners to the parent element and only the li elements will be draggable
+        if(e.target.tagName == 'LI'){
+            initialX = e.touches[0].clientX;
+            initialY = e.touches[0].clientY;
+            e.target.classList.add('dragged');
+        }
+    });
+    draggableItemsContainer.addEventListener('touchmove', (e) => {
+        if(e.target.tagName == 'LI'){
+            const x = e.touches[0].clientX - initialX;
+            const y = e.touches[0].clientY - initialY;
+            lastX = e.touches[0].clientX;
+            lastY = e.touches[0].clientY;
+            e.target.style.transform = "translate(" + x + "px, " + y + "px)";
+
+            const elementList = document.elementsFromPoint(lastX, lastY);
+            //if(elementList.length !== 4 && elementList.length > 0){
+            if(elementList.length > 4){
+                if(!elementList[1].classList.contains('dragover')){
+                    elementList[1].classList.add('dragover');
+                }
+            } else {
+                let list = document.getElementsByClassName('dragover');
+                if(list.length > 0){
+                    list[0].classList.remove('dragover');
+                }
+            }
+        }
+    });
+    draggableItemsContainer.addEventListener('touchend', (e) => {
+        if(e.target.tagName == 'LI'){
+            const elementList = document.elementsFromPoint(lastX, lastY);
+            if (elementList.length > 1 && elementList[1].hasAttribute('draggable')) {
+                // die swapItems Funktion wurde bereits in Aufgabe 1b von Ihnen erstellt
+                swapItems(e.target.dataset.index, elementList[1].dataset.index);
+            }
+            //e.target.style.transform = "translate(0px, 0px)";
+            e.target.style.transform = "";
+            e.target.classList.remove('dragged');
+            elementList[1].classList.remove('dragover');
+        }
+    });
+}
+
 
 // check if javascript gets called on mobile
 function detectMob() {
